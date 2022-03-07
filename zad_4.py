781221bbff66657c4a2a370b1978b8715bb84fc8
malamare2 <mberos@pmfst.hr>
2022-03-07 19:42:57 +0100

    zad4

--- zad_4.py
+++ zad_4.py
@@ -1,12 +1,15 @@
-x1 = int(input(""))
-y1 = int(input(""))
-x2 = int(input(""))
-y2 = int(input(""))
-    
 def pravac():
-
-    a = (y2-y1)/(x2-x1)
-    b = y1-a*x1
-    print("y =",a,"x +",b)
+    
+    x1 = input("")
+    y1 = input("")
+    x2 = input("")
+    y2 = input("")
+    
+    if x1.isdigit() and y1.isdigit() and x2.isdigit() and y2.isdigit():
+        a = (int(y2)-int(y1))/(int(x2)-int(x1))
+        b = int(y1)-a*int(x1)
+        print("y =",a,"x +",b)
+    else:
+        print("ponovi")
 
 pravac()
