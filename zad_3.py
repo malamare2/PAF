994fa60bdce16c3eee1ace0f182f0f394b801ae1
malamare2 <mberos@pmfst.hr>
2022-03-07 19:42:34 +0100

    zad3

--- zad_3.py
+++ zad_3.py
@@ -1,9 +1,11 @@
-x1 = int(input(""))
-y1 = int(input(""))
-x2 = int(input(""))
-y2 = int(input(""))
+x1 = input("")
+y1 = input("")
+x2 = input("")
+y2 = input("")
 
-a = (y2-y1)/(x2-x1)
-b = y1-a*x1
-
-print("y =",a,"x +",b)
+if x1.isdigit() and y1.isdigit() and x2.isdigit() and y2.isdigit():
+    a = (int(y2)-int(y1))/(int(x2)-int(x1))
+    b = int(y1)-a*int(x1)
+    print("y =",a,"x +",b)
+else:
+    print("ponovi")
