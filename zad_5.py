33049c99fd7dc82519f40cb997b65d74460c4a3d
malamare2 <mberos@pmfst.hr>
2022-03-07 19:43:15 +0100

    zad5

--- zad_5.py
+++ zad_5.py
@@ -1,17 +1,33 @@
 import matplotlib.pyplot as plt
 
-x1 = int(input(""))
-y1 = int(input(""))
-x2 = int(input(""))
-y2 = int(input(""))
-
 def pravac():
+    
+    x1 = input("")
+    y1 = input("")
+    x2 = input("")
+    y2 = input("")
+    
+    if x1.isdigit() and y1.isdigit() and x2.isdigit() and y2.isdigit():
+        a = (int(y2)-int(y1))/(int(x2)-int(x1))
+        b = int(y1)-a*int(x1)
+        print("y =",a,"x +",b)
 
-    a = (y2-y1)/(x2-x1)
-    b = y1-a*x1
-    return("y =",a,"x +",b)
+        c = "slika"
+        d = "PDF"
+        print("Želite li",c,"ili",d)
+        f = input("")
+
+        if f == c:
+            plt.plot([x1,x2],[y1,y2])
+            plt.show()
+        elif f == d:
+            plt.plot([x1,x2],[y1,y2])
+            plt.savefig(input("ime: "), dpi='figure', format=None, metadata=None, bbox_inches=None, pad_inches=0.1, facecolor='auto', edgecolor='auto',backend=None)
+        else:
+            print("ponovi")
+
+    else:
+        print("ponovi")
 
 pravac()
 
-plt.plot([x1,y1],[x2,y2])
-plt.show()
