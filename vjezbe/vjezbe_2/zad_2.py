c8ad2ebfdb1be6c064c7d9ab6d3c8d5ecccdadd9
malamare2 <mberos@pmfst.hr>
2022-03-18 11:04:11 +0100

    vjezbe 3 sa sata

--- vjezbe/vjezbe_2/zad_2.py
+++ vjezbe/vjezbe_2/zad_2.py
@@ -2,42 +2,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-theta = 60
 
-t = 10
-dt = 0.01
-g = 9.81
-v0 = 5
+def kosi_hitac(theta,v0):
 
-v0x = v0*math.cos((theta/360)*2*np.pi)
-v0y = v0*math.sin((theta/360)*2*np.pi)
+    t = 10
+    dt = 0.01
+    g = 9.81
 
-y0 = 0
-x0 = 0
-t0 = 0
 
-X = []
-Y = []
-T = []
-V = []
+    v0x = v0*math.cos((theta/360)*2*np.pi)
+    v0y = v0*math.sin((theta/360)*2*np.pi)
 
-for i in range(100):
-    t0 = t0 + dt   #na staro vrijeme dodajem novo vrijeme
+    y0 = 0
+    x0 = 0
+    t0 = 0
 
-    v0y = v0y-g*dt   #na brzinu na y-osi oduziman joj g i promjenu vremena i to apendam u praznu listu kako bi mi se mjenjalo za svaki mali dio
-    V.append(v0y)
+    X = []
+    Y = []
+    T = []
+    V = []
 
-    x0 = x0+v0x*dt   #pomicanje po x-osi
-    X.append(x0)
+    for i in range(100):
+        t0 = t0 + dt   #na staro vrijeme dodajem novo vrijeme
 
-    y0 = y0 + v0y*dt  #pomicanje po y-osi
-    Y.append(y0)
-    T.append(t0)
+        v0y = v0y-g*dt   #na brzinu na y-osi oduziman joj g i promjenu vremena i to apendam u praznu listu kako bi mi se mjenjalo za svaki mali dio
+        V.append(v0y)
 
-plt.subplot(1,3,1)
-plt.plot(T,Y)
-plt.subplot(1,3,2)
-plt.plot(T,X)
-plt.subplot(1,3,3)
-plt.plot(X,Y)
-plt.show()
+        x0 = x0+v0x*dt   #pomicanje po x-osi
+        X.append(x0)
+
+        y0 = y0 + v0y*dt  #pomicanje po y-osi
+        Y.append(y0)
+        T.append(t0)
+
+    fig = plt.subplots(1,3,figsize = (13,4))
+    plt.subplots_adjust(
+        bottom = 0.1, right = 0.95, left = 0.1, top = 0.9
+    )
+    plt.subplot(1,3,1)
+    plt.plot(T,Y)
+    plt.subplot(1,3,2)
+    plt.plot(T,X)
+    plt.subplot(1,3,3)
+    plt.plot(X,Y)
+    plt.show()
+
+kosi_hitac(60,5)
