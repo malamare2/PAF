3003f737d76621d88e023da4b756d37867055a60
malamare2 <mberos@pmfst.hr>
2022-03-27 22:58:37 +0200

    vjezbe 3

--- vjezbe/vjezbe_3/gibanje.py
+++ vjezbe/vjezbe_3/gibanje.py
@@ -1,7 +1,28 @@
 import particle as prt
+import matplotlib.pyplot as plt
 
-p1 = prt.Particle(10, -5)
-p1.printInfo()
+# odstupanje u gibanju
 
-p1.mass = 15
-p1.printInfo()
+p1 = prt.Particle(0, 0, 40, 10)
+p1.set_initial_conditions(40, 10)
+
+odstupanje = abs(((p1.analiticki(40) - p1.range(0.01)))/p1.analiticki(40))*100
+print("odstupanje je:",odstupanje)
+
+
+# graf greške
+
+p1 = prt.Particle(0, 0, 60, 10)
+p = []
+pogreska = []
+    
+for i in range(1000):
+    p1.set_initial_conditions(60, 10)
+    promjena = i * 0.001 + 0.001
+    p.append(promjena)
+    pogreska1 = abs(((p1.analiticki(60) - p1.range(promjena)))/p1.analiticki(40))*100
+    pogreska.append(pogreska1)
+    p1.reset()
+
+plt.plot(p, pogreska)
+plt.show()
--- vjezbe/vjezbe_3/particle.py
+++ vjezbe/vjezbe_3/particle.py
@@ -1,9 +1,11 @@
 import math
 import numpy as np
+import matplotlib.pyplot as plt
+from math import *
 
 
 class Particle:
-    def __init__ (self):
+    def __init__ (self, x0, y0, theta, v0):
         self.x = []
         self.y = []
         self.vx = []
@@ -11,12 +13,48 @@ class Particle:
         self.ax = []
         self.ay = []
         self.t = []
-        self.dt = 0.1
-    
-    def set_initial_conditions(self, x0, y0, theta, v0):
-        self.vx = v0*math.cos((theta/360)*2*np.pi)
-        self.vy = v0*math.sin((theta/360)*2*np.pi)
-        self.x = x0
-        self.y = y0
-        self.t = 0
         
+        self.g = 9.81
+        self.x0 = x0
+        self.y0 = y0
+        self.theta = theta
+        self.v0 = v0
+    
+    def set_initial_conditions(self, theta, v0):
+        self.vx.append(v0*math.cos((theta/360)*2*np.pi))
+        self.vy.append(v0*math.sin((theta/360)*2*np.pi))
+        self.x.append(0)
+        self.y.append(0)
+        self.t.append(0)
+        self.ax.append(0)
+        self.ay.append(9.81)
+
+    def reset(self):
+        self.vx = []
+        self.vy = [] 
+        self.x = []
+        self.y = []
+
+    def __move(self, dt):
+        self.t.append(self.t[-1]+dt)
+        self.vy.append(self.vy[-1]-self.g*dt)
+        self.vx.append(self.vx[-1]+self.ax[-1]*dt)
+        self.x.append(self.x[-1]+self.vx[-1]*dt)
+        self.y.append(self.y[-1]+self.vy[-1]*dt)
+
+    def range(self, dt):
+        while (self.y[-1] >= 0):
+            self.__move(dt)
+        return (self.x[-1])
+
+    def plot_trajectory(self):
+        plt.plot(self.x, self.y)
+        plt.show()
+
+    def analiticki(self, theta):
+        g = 9.81
+        D = (self.v0**2)*sin(2*((theta/360)*2*np.pi))/g
+        return D
+
+    
+    
