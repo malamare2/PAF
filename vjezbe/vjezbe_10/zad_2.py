718f85321e718943e57fd01d96dd0292df3b48b1
malamare2 <mberos@pmfst.hr>
2022-05-28 21:53:15 +0200

    vjezba 10

--- vjezbe/vjezbe_10/zad_2.py
+++ vjezbe/vjezbe_10/zad_2.py
@@ -3,10 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 p1 = prt.Particle()
-p1.set_initial_conditions(np.array([0., 0., 0.]), np.array([0., 0., 1.]), np.array([0.1, 0.1, 0.1]), 1, 1)
 p1.set_initial_conditions(np.array([0., 0., 0.]), np.array([0., 0., 1.]), np.array([0.1, 0.1, 0.1]), -1, 1)
 x1, y1, z1 = p1.range(10)
-x2, y2, z2 = p1.range(10)
+p2 = prt.Particle()
+p2.set_initial_conditions(np.array([0., 0., 0.]), np.array([0., 0., 1.]), np.array([0.1, 0.1, 0.1]), -1, 1)
+x2, y2, z2 = p2.runge(10)
 
 ax = plt.axes(projection='3d')
 ax.plot3D(x1, y1, z1)
